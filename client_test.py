6578fe310656a13966feb754c07bf568f9a6cbc7
tejaswigharkar <79370438+tejaswigharkar@users.noreply.github.com>
2023-09-14 08:11:20 +0530

    client_test.py

--- client_test.py
+++ client_test.py
@@ -7,17 +7,32 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    for i in quotes:
+      self.assertEqual(getDataPoint(i),i['stock'],i[bid_price], i['ask_price'], (i['bid_price']+i['ask_price']/2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+   for i in quotes:
+      self.assertEqual(getDataPoint(i), (i['stock'], i['top_bid']['price'], i['top_ask']['price'], (i['top_bid']['price'] + i['top_ask']['price']) / 2))
 
+  def test_getDataPoint_calculatePriceWithNoBids(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {}, 'id': '0.109974697771', 'stock': 'XYZ'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {}, 'id': '0.109974697771', 'stock': 'PQR'}
+    ]
+    for i in quotes:
+      self.assertEqual(getDataPoint(i), (i['stock'], None, i['top_ask']['price'], None))
 
-  """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_calculatePriceWithNoAsks(self):
+    quotes = [
+      {'top_ask': {}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'LMN'},
+      {'top_ask': {}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'QRS'}
+    ]
+    for i in quotes:
+      self.assertEqual(getDataPoint(i), (i['stock'], i['top_bid']['price'], None, None))
 
 
 
